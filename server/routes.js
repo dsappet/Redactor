cf7d51da351b7d1076d07aca80ba15f21807ef6d
Douglas Sappet <dsappet@gmail.com>
2018-04-21 15:42:32 -0400

    Express server setup
    Basic node server is working, use node server.js to run it

---
+++ server/routes.js
@@ -0,0 +1,20 @@
+// app/routes.js
+
+
+module.exports = function(app) {
+
+    // server routes ===========================================================
+    // handle things like api calls
+    // authentication routes
+
+    // route to handle get goes here (app.get)
+    // route to handle creating goes here (app.post)
+    // route to handle delete goes here (app.delete)
+
+    // frontend routes =========================================================
+    // route to handle all angular requests
+    app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load our public/index.html file
+    });
+
+};
