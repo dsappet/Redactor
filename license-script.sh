cfcd83c70e53299b3ccc25ec89b3c3edbaddc5d9
Douglas Sappet <dsappet@gmail.com>
2018-05-25 14:18:31 -0400

    updating license script

--- license-script.sh
+++ license-script.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 #
 
-echo "executing license script"
-yarn licenses list 
+echo "executing license script!!"
+OUTPUT="$(yarn licenses list)"
+echo "${OUTPUT}"
+
+echo "Attempting to push code to server"
